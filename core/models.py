0e56e591d408d0ccc9f2e06af3e581c10308ef60
rajbelwal <99012947+rajbelwal@users.noreply.github.com>
2022-06-23 09:59:31 +0530

    Add files via upload

---
+++ core/models.py
@@ -0,0 +1,51 @@
+from distutils.command.upload import upload
+from email.policy import default
+from django.db import models
+from django.contrib.auth import get_user_model
+import uuid
+from datetime import datetime
+from django.forms import UUIDField
+
+
+User = get_user_model()
+
+# Create your models here
+
+
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    id_user = models.IntegerField()
+    bio = models.TextField(blank=True)
+    profileimg = models.ImageField(upload_to='profile_images', default='5.jpg')
+    location = models.CharField(max_length=100, blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+
+class Post(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    user = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='post_images')
+    caption = models.TextField(max_length=200)
+    created_at = models.DateTimeField(default=datetime.now)
+    no_of_likes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.user
+
+
+class LikePost(models.Model):
+    post_id = models.CharField(max_length=100)
+    username = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.username
+
+
+class FollowerCount(models.Model):
+    follower = models.CharField(max_length=100)
+    user = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.user
